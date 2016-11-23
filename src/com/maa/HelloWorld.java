353031d0ea45e8ec49a6145f792aefe3696f7b27
srivani padavala <srivanipadavala@srivanis-MacBook-Pro.local>
2016-11-22 20:58:55 -0800

    initial commit

---
+++ src/com/maa/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.maa;
+
+public class HelloWorld {
+
+	public static void main(String[] args){
+		System.out.println("Hello World!!!");
+		System.out.println("Hello Ananya and Akhil!!!");
+		System.out.println("1+2="+(1+2));
+	}
+		
+	
+}
